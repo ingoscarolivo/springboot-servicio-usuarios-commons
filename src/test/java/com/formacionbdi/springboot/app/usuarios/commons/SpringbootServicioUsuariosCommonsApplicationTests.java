17a6cd644983daab5184ede3cf1f7dc099491f5b
Oscar Olivo <oscar.olivo@ceiba.com.co>
2023-03-07 17:04:01 -0500

    first commit

---
+++ src/test/java/com/formacionbdi/springboot/app/usuarios/commons/SpringbootServicioUsuariosCommonsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.formacionbdi.springboot.app.usuarios.commons;
+
+import com.formacionbdi.springboot.app.commons.usuarios.SpringbootServicioUsuariosCommonsApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SpringbootServicioUsuariosCommonsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest
+class SpringbootServicioUsuariosCommonsApplicationTests {
+
+	//@Test
+	//void contextLoads() {
+	//}
+
+}
